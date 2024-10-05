4d1dec13d121aca91d3f1498858d3497895385a2
Luis Felipe Salazar Ucros <40307832+luisfucros@users.noreply.github.com>
2024-10-05 18:52:41 +0000

    first versions app

---
+++ app/app.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const connectDB = require('./db/connect')
+const express = require('express')
+
+app = express()
+
+
+const port = process.env.PORT || 5000
+
+const start = async() => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () => 
+        console.log(`Server is listening on port ${port}...`)
+        )
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+start()
