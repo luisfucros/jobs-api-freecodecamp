f1268beaaff1aea86243963f3c743342712873db
Luis Felipe Salazar Ucros <40307832+luisfucros@users.noreply.github.com>
2024-10-20 20:44:52 +0000

    update file

--- app/app.js
+++ app/app.js
@@ -1,7 +1,18 @@
 require('dotenv').config()
 require('express-async-errors')
-const express = require('express')
 
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+// const rateLimiter = require('express-rate-limit')
+
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+const express = require('express')
 app = express()
 
 const connectDB = require('./db/connect');
@@ -15,15 +26,28 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
+// app.use(
+//     rateLimiter({
+//       windowMs: 15 * 60 * 1000, // 15 minutes
+//       max: 100, // limit each IP to 100 requests per windowMs
+//     })
+//   )
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
-app.get("/", (req, res) => {
-    res.json({status: 'ok'})
-  })
+app.get('/', (req, res) => {
+    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+  });
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
 
